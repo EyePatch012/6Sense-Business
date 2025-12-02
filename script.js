e7585874f55e8097e140f936abbb6deef4f3b0ea
Eye Patch <rashidanjum012@gmail.com>
2025-12-02 01:49:20 +0100

    67

--- script.js
+++ script.js
@@ -2,7 +2,15 @@
 const searchBtn = document.getElementById("searchBtn");
 const searchInput = document.getElementById("searchInput");
 
-
 searchBtn.addEventListener("click", function () {
-	alert("You searched for: " + searchInput.value);
+    alert("You searched for: " + searchInput.value);
+});
+
+// INTRO SCREEN FADE OUT AFTER 5 SECONDS
+window.addEventListener("load", () => {
+    const intro = document.getElementById("intro-screen");
+
+    setTimeout(() => {
+        intro.classList.add("fade-out");
+    }, 5000); // fade after 5 seconds
 });
